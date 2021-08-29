63730de6dcc3862873b39a1324aa8cb640e838d4
Brandon Javillo <bjavillo1059@gmail.com>
2021-08-29 11:48:48 -0700

    employee.test complete with total pass

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -3,29 +3,37 @@ const Employee = require('../lib/Employee');
 
 describe ("inilization", () => {
     it("should create an employee object if presented with valid arguments", () => {
-        const employee = new Employee ("Brandon", "1", "brandonjavillo@gmail.com")
+        const employee = new Employee ("Brandon", "1", "test@test.com")
         expect(employee.name).toEqual("Brandon")
-        expect(employee.id).toEqual("4")
-        expect(employee.email).toEqual("br@gmail.com")
+        expect(employee.id).toEqual("1")
+        expect(employee.email).toEqual("test@test.com")
     });
 
     it("should return employee name with getName function", () => {
-        const employee = new Employee ("Brandon", "1", "brandonjavillo@gmail.com")
+        const employee = new Employee ("Brandon", "1", "test@test.com")
         expect(employee.getName()).toEqual("Brandon")
     });
 
     it("should return employee id with getId function", () => {
-        const employee = new Employee ("Brandon", "1", "brandonjavillo@gmail.com")
+        // const employCount = 50 // trying to set a max limit count for employees / broken will study more and fix later
+        const employee = new Employee ("Brandon", "1", "test@test.com")
         expect(employee.getId()).toEqual("1")
+        // expect(employee.getId()).toEqual(number) // test for max limit of employees / broken will study more and fix later
     });
 
     it("should return employee email with getEmail function", () => {
-        const employee = new Employee ("Brandon", "1", "brandonjavillo@gmail.com")
-        expect(employee.getEmail()).toEqual("brandonjavillo@gmail.com") 
+        const employee = new Employee ("Brandon", "1", "test@test.com")
+        expect(employee.getEmail()).toEqual("test@test.com") 
     });
+    
+    it("should return employee rold with getRole function", () => {
+        let testRole = "Employee"
+        const employee = new Employee ("Brandon", "1", "test@test.com")
+        expect(employee.getRole()).toEqual(testRole)
+    })
 
     it("should return employee object information with Employee class", () => {
-        const employee = new Employee ("Brandon", "1", "brandonjavillo@gmail.com")
-        expect(Employee()).toEqual(Employee)
+        const employee = new Employee ("Brandon", "1", "test@test.com")
+        expect(typeof(employee)).toEqual("object")
     })
 })
--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,4 +1,13 @@
 const { describe } = require("yargs");
 const Manager = require("../lib/Manager");
 
-describe("")
+describe("Manager", () => {
+    it("should return manager office number set by constructor", () => {
+        const testNum = 1234567890;
+        const e = new Manager("Boss", 1, "test@test.com", testNum);
+        expect(e.officeNumber).toEqual(testNum);
+    });
+
+    it("should return")
+
+})
