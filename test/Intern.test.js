bed68bf0d20cedc84c40c18ea16d94db4ed40f44
Brandon Javillo <bjavillo1059@gmail.com>
2021-08-29 13:31:39 -0700

    intern.test complete with pass

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -2,5 +2,21 @@
 const Intern = require("../lib/Intern")
 
 describe("Intern", () => {
-    it("should return school name or institution set by constructor", )
+    it("should return school name or institution set by constructor", () => {
+        const testEd = "UC Davis";
+        const e = new Intern("NoobSauce", 3, "test@test.com", testEd);
+        expect(e.education).toEqual(testEd);
+    });
+
+    it("should return 'Intern' role with getRole function", () => {
+        const testInRole = "Intern";
+        const e = new Intern("NoobSauce", 3, "test@test.com", "UC Davis");
+        expect(e.getRole()).toEqual(testInRole);
+    });
+
+    it("should return institution with the getEducation function", () => {
+        const testEd = "UC Davis";
+        const e = new Intern("NoobSauce", 3, "test@test.com", testEd);
+        expect(e.getEducation()).toEqual(testEd);
+    })
 });
